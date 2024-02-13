f4f17a700037da0218ee84dd73316377e0aba906
ColinAdo <colinadodev30@gmail.com>
2024-02-13 17:34:01 +0300

    Add Category model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from products.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "display_image", "date"]
+
+    def display_image(self, obj):
+        return mark_safe('<a href="{}"> <img src="{}" width="30" height="30" style="border-radius: 50%;" /> </a>'.format(obj.image.url, obj.image.url))
+    display_image.short_description = 'Image'
+
+
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, default="Chairs")
+    image = models.ImageField(upload_to='category', default='category.png')
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
