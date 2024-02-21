7775521346c630730b0b68f3ebed30987e923a8a
ColinAdo <colinadodev30@gmail.com>
2024-02-21 20:19:15 +0300

    Update image directory path

--- accounts/models.py
+++ accounts/models.py
@@ -5,13 +5,16 @@ from django.db import models
 def user_directory_path(instance, filename):
     return "profile/{0}/{1}".format(instance.username, filename)
 
+def vendor_directory_path(instance, filename):
+    return "Vendor/{0}/{1}".format(instance.name, filename)
+
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(max_length=200, default="profile.png", upload_to=user_directory_path)
     
 class Vendor(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to=user_directory_path, default='venders.jpg')
+    image = models.ImageField(upload_to=vendor_directory_path, default='venders.jpg')
     description = models.TextField(null=True, blank=True)
     contact = models.CharField(max_length=100, default='(+254) 123 456 678')
     address = models.CharField(max_length=100, default='P.0 Box Nairobi')
@@ -23,4 +26,4 @@ class Vendor(models.Model):
         verbose_name_plural = 'Venders'
 
     def __str__(self):
-        return self.title
+        return self.name
