d5661541841e85725033359b6745cd16633ae8ec
ColinAdo <colinadodev30@gmail.com>
2024-02-21 20:21:30 +0300

    Add URLConf

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from products.models import Product
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
