3435e81fec80842c823ded8049773410a5d2cdaa
ColinAdo <colinadodev30@gmail.com>
2024-02-28 19:11:26 +0300

    Add product quereyset

--- products/views.py
+++ products/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from products.models import Product, Category
+from products.models import Product, Category, ProductReview
 
 class ProductListView(ListView):
     model = Product
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by("-date_published")
     template_name = 'products/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+
+        context['top_rated'] = ProductReview.objects.filter(rating=5)
         return context
