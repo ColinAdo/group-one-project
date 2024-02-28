7b4f272462d19f61f92af78bc864c5eecea458a5
ColinAdo <colinadodev30@gmail.com>
2024-02-28 20:14:42 +0300

    Add category products URLConf

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import ProductListView
+from products.views import ProductListView, CategoryProductList
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="index"),
+    path("category/<int:pk>/products/",CategoryProductList.as_view(), name="category"),
 ]
