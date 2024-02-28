aea2f897751c511cbc1e9506f41fa7a48df00495
ColinAdo <colinadodev30@gmail.com>
2024-02-28 20:14:09 +0300

    Add category products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -14,3 +16,19 @@ class ProductListView(ListView):
 
         context['top_rated'] = ProductReview.objects.filter(rating=5)
         return context
+    
+class CategoryProductList(ListView):
+    model = Product
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('pk')
+        return Product.objects.filter(category__id=category_id)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('pk')
+        context['categories'] = Category.objects.all()
+        context['category'] = Category.objects.get(pk=category_id)
+        return context
+    
+        
