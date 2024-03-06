c44805bb264c675a8584c03a050cc13160e7a405
ColinAdo <colinadodev30@gmail.com>
2024-03-06 09:20:57 +0300

    Add product details

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from products.views import ProductListView, CategoryProductList
+from products.views import ProductListView, CategoryProductList, ProductDetail
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="index"),
+    path("product/<int:pk>/detail/", ProductDetail.as_view(), name="product"),
     path("category/<int:pk>/products/",CategoryProductList.as_view(), name="category"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from products.models import Product, Category, ProductReview
 
@@ -31,4 +31,13 @@ class CategoryProductList(ListView):
         context['category'] = Category.objects.get(pk=category_id)
         return context
     
-        
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'products/product-detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
+
