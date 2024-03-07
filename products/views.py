4f97f72d4f092401074307923fb7258cae6c62ff
ColinAdo <colinadodev30@gmail.com>
2024-03-07 10:10:00 +0300

    Add category dropdown

--- products/views.py
+++ products/views.py
@@ -101,6 +101,7 @@ class ShoppingCart(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
         context['carts'] = CartOrder.objects.filter(user=self.request.user, checked_out=False)
 
         carts_queryset = CartOrder.objects.filter(user=self.request.user, checked_out=False)
