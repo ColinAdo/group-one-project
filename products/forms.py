44ecfb900353164bc9b7e3aa5932e8d3656df44c
ColinAdo <colinadodev30@gmail.com>
2024-03-07 09:36:10 +0300

    Add checkout form

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import CartOrder
+from .models import CartOrder, Checkout
 
 
 class CartOrderForm(forms.ModelForm):
     class Meta:
         model = CartOrder
         fields = ['quantity']
+
+
+class CheckoutForm(forms.ModelForm):
+    class Meta:
+        model = Checkout
+        fields = ['fname', 'lname', 'address', 'city', 'phone', 'email']
