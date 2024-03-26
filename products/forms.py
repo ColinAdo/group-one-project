595e04b8b31f77d1f697a4b4e2a3200fa9af7208
ColinAdo <colinadodev30@gmail.com>
2024-03-26 07:50:16 +0300

    Add product form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CartOrder, Checkout
+from .models import CartOrder, Checkout, Product
 
 
 class CartOrderForm(forms.ModelForm):
@@ -12,3 +12,18 @@ class CheckoutForm(forms.ModelForm):
     class Meta:
         model = Checkout
         fields = ['fname', 'lname', 'address', 'city', 'phone', 'email']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'image',    
+            'category', 
+            'name',
+            'description',
+            'price',
+            'product_type',
+            'number_in_stock',
+            'in_stock',
+
+        ]
