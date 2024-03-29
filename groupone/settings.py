b4492e54118bc1eb5188a1f6ba9f8b21dd3283a7
ColinAdo <colinadodev30@gmail.com>
2024-03-29 09:25:18 +0300

    Add clean code to settings

--- groupone/settings.py
+++ groupone/settings.py
@@ -5,9 +5,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7i_#q9^(0br*%^lbea1yz#2!f7^@n**gm+auyi=%cjg)!szz!0'
 
